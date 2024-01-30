15092fe44ec90df8d6a49583d06f5f77327af14b
MAPAKAC123 <Kopylovmaxim999@gmail.com>
2024-01-31 01:41:34 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PR_31_Tur"
+include(":app")
